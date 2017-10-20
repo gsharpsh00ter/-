cab5755fe26e4e4544828628c6fa8647dfdf9106
guziqiang <guziqiang>
2017-10-20 16:01:45 +0800

    Add bind9 configuration files

---
+++ bind9/etc/mkdir.sh
@@ -0,0 +1,6 @@
+mkdir -p /home/g/bind9/var/run
+mkdir -p /home/g/bind9/var/named
+mkdir -p /home/g/bind9/var/named/dynamic
+mkdir -p /home/g/bind9/var/run/data
+mkdir -p /home/g/bind9/var/named/data
+mkdir -p /home/g/bind9/var/named/log
